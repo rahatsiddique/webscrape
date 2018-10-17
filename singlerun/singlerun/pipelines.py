22f449dce3fab7a62186dfcf6578e2a18fdb4e38
Rahat Siddique <rahat.k.siddique@gmail.com>
2018-10-17 20:50:45 +0100

    now has variable names in csv file

---
+++ singlerun/singlerun/pipelines.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import csv
+
+class SinglerunPipeline(object):
+    def process_item(self, item, spider):
+        self.numberwriter.writerow([item["article"]])
+        return item
+
+    def open_spider(self,spider):
+        self.file= open('file1.csv', 'w', encoding = "utf-16")
+        self.numberwriter = csv.writer(self.file, dialect="excel-tab")
+        self.numberwriter.writerow(["article titles", "author", "date"])
+
+
+    def close_spider(self, spider):
+        self.file.close()
+
+
+#utf-16 works with the excel on my laptop, in general this can be utf-8 but doens't work for excel for mac :(
