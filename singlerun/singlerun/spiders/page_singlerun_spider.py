762c97559a95a8d914708eda2ebc5bcd5d7c267a
Rahat Siddique <rahat.k.siddique@gmail.com>
2018-10-17 20:40:09 +0100

    the files will only run once

--- singlerun/singlerun/spiders/page_singlerun_spider.py
+++ singlerun/singlerun/spiders/page_singlerun_spider.py
@@ -5,12 +5,12 @@ class PageSpider(scrapy.Spider):
     start_urls = ["https://blog.prolific.ac/"]
 
     def parse(self, response):
-        for article_url in response.css('.entry-title a ::attr("href")').extract():
+        for article_url in response.css('h2 a ::attr("href")').extract():
             yield response.follow(article_url, callback=self.parse_article)
         older_posts = response.css('.nav-previous a ::attr("href")').extract_first()
         if older_posts is not None:
             yield response.follow(older_posts, callback=self.parse)
 
     def parse_article(self, response):
-        content = response.xpath(".//div[@class='entry-content']/descendant::text()").extract()
+        content = response.css("h1::text").extract()
         yield {"article": "".join(content)}
