fd05182909e5006a35c0ea3d8e4a328e2922b1ae
Rahat Siddique <rahat.k.siddique@gmail.com>
2018-10-24 20:57:03 +0100

    tried to scrape multiple items

---
+++ singlerun/singlerun/spiders/multi_page_single_run_spider.py
@@ -0,0 +1,25 @@
+import scrapy
+
+class PageSpider(scrapy.Spider):
+    name = "page"
+    start_urls = ["https://blog.prolific.ac/"]
+
+    def parse(self, response):
+        for article_url in response.css('h2 a ::attr("href")').extract():
+            author = response.css('span a ::attr("href")').extract()
+            date = response.css('span time ::attr("datetime")').extract()
+
+            article = response.follow(article_url, callback=self.parse_article)
+            yield {"article": article, "author": author, "date": date}
+
+        older_posts = response.css('.nav-previous a ::attr("href")').extract_first()
+        if older_posts is not None:
+            yield response.follow(older_posts, callback=self.parse)
+
+    def parse_article(self, response):
+        content = response.css("h1::text").extract()
+        yield "".join(content)
+
+
+
+#in the command line write 'scrapy crawl page' because it is the name attribute
